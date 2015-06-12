73650e8af3fc88542ea71e060d1a635d3c96aec8
Paal Joachim Romdahl <paaljoachim@hotmail.com>
2015-06-13 01:52:57 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
-//* This is code for adding a shortcode instead of adding html directly to a text widget.
+/*------------ Adding a Dugnad shortcode --------------*/
+
 /*
 Here's a super simple way to turn our dugnadskalkulator into a shortcode.
 I'm kinda conflicted as to whether this would actually be useful, but as a demo it's ok.
@@ -47,43 +48,43 @@ function dugnad_shortcode() {
 	ob_start(); // Here's a cool way to do markup inside our function
 	?>
 	<div id="calc-form">
-    <!-- <form oninput="updateRanges()"> -->
-<form oninput="updateRanges()" onchange="updateRanges()">  <!-- For IE--->       	                                        
-              <div id="calc-heading">Dugnadskalkulator</div>                           
-              
-         <div id="calc-content">                                  
-                      <div class="calcslider">Hvor mye penger ønsker gruppen å tjene? 
-                       <div class="styled-output"><output for="r10">1400</output>kr</div> 
-                                       
-                                       <input type="range" name="r10" step="1400" min="1400" max="160000" value="0">
-                      </div>  
-
-		  <div class="resultat-salgprodukter">Antall produkter å selge: <span>20</span> <br /></div>                                 
-                        <div class="calcslider">Velg antall deltagere i gruppen: <br /></div>
-                         <div class="styled-output"><output for="r20">1</output></div> 
-                                       
-                                       <input type="range" name="r20" step="1" min="1" max="40" value="1">
-                    
-               <div class="resultat-salgdeltager">Antall bursdagsesker pr. deltager: <br><span>20</span></div>
-                          
-               <div id="calc-sammendrag-heading">Sammendrag</div>
-               <div class="calc-sammendrag">
-                                  
-               		<p>
-               		<div class="resultat-salgprodukter">
-                  		 Antall Produkter Totalt: <span>20</span> 
-               		</div>
-                                  
-               		<div class="resultat-kartonger">
-               		     Antall Kartonger a 20 Esker: <span>1</span>
-              		</div>
-                                  
-              			 Fortjeneste pr. Produkt 70 Kr
-              		<br />Anbefalt utsalgspris 130kr
-               </div>
-          </div>                           
-    </form>
-</div>
+	    <!-- <form oninput="updateRanges()"> -->
+	<form oninput="updateRanges()" onchange="updateRanges()">  <!-- For IE--->       	                                        
+	              <div id="calc-heading">Dugnadskalkulator</div>                           
+	              
+	         <div id="calc-content">                                  
+	                      <div class="calcslider">Hvor mye penger ønsker gruppen å tjene? 
+	                       <div class="styled-output"><output for="r10">1400</output>kr</div> 
+	                                       
+	                                       <input type="range" name="r10" step="1400" min="1400" max="160000" value="0">
+	                      </div>  
+	
+			  <div class="resultat-salgprodukter">Antall produkter å selge: <span>20</span> <br /></div>                                 
+	                        <div class="calcslider">Velg antall deltagere i gruppen: <br /></div>
+	                         <div class="styled-output"><output for="r20">1</output></div> 
+	                                       
+	                                       <input type="range" name="r20" step="1" min="1" max="40" value="1">
+	                    
+	               <div class="resultat-salgdeltager">Antall bursdagsesker pr. deltager: <br><span>20</span></div>
+	                          
+	               <div id="calc-sammendrag-heading">Sammendrag</div>
+	               <div class="calc-sammendrag">
+	                                  
+	               		<p>
+	               		<div class="resultat-salgprodukter">
+	                  		 Antall Produkter Totalt: <span>20</span> 
+	               		</div>
+	                                  
+	               		<div class="resultat-kartonger">
+	               		     Antall Kartonger a 20 Esker: <span>1</span>
+	              		</div>
+	                                  
+	              			 Fortjeneste pr. Produkt 70 Kr
+	              		<br />Anbefalt utsalgspris 130kr
+	               </div>
+	          </div>                           
+	    </form>
+	</div>
 	<?php
 	
 	return ob_get_clean(); // return markup
@@ -98,3 +99,5 @@ Really, the hardest part of all this is where to put this stuff. If we add it di
 The most sensible thing would be to wrap it all up in a tiny custom plugin. If we were to "mu" the thing, it wouldn't be too much of a hazzle. Thoughts?
 */
 
+
+/*---------- Dugnad Shortcode END - Better to have this in a custom functions plugin --------------*/
